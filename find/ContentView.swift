9198a31a81dc5eee8703d34f95792f8860701557
farah alshammari <farah.ntuf@outlook.sa>
2023-12-18 12:51:29 +0300

    Initial Commit

---
+++ find/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  find
+//
+//  Created by Farah mohammed alshammari on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ find/findApp.swift
@@ -0,0 +1,17 @@
+//
+//  findApp.swift
+//  find
+//
+//  Created by Farah mohammed alshammari on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct findApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
